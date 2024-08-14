1c949f6188c147fb59d45aade0848a8c59ff4b47
ken <kenschlesiger@gmail.com>
2024-08-14 15:26:06 +0200

    nvim

--- init.lua
+++ init.lua
@@ -1,4 +1,11 @@
 require("config.lazy")
 require("config.set")
+require("config.remap")
 vim.cmd ":colorscheme catppuccin-macchiato"
 require("mason").setup()
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
---
+++ lua/config/remap.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>z", ":!zathura <C-r>=expand('%:r')<cr>.pdf &<cr>")
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', {noremap = true})
+vim.keymap.set("n", "<leader>en", ":setlocal spell spelllang=en <cr>")
+vim.keymap.set("n", "<leader>de", ":setlocal spell spelllang=de <cr>")
